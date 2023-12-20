9e1e715709825be8ea71c7899f2902f64735261b
Scott Crowder <scrowder@gmail.com>
2023-12-19 22:07:34 -0600

    save and retrieve posts

--- board/posts.py
+++ board/posts.py
@@ -1,15 +1,39 @@
-from flask import Blueprint, render_template
+from flask import (
+    Blueprint,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+
+from database import get_db
 
 bp = Blueprint("posts", __name__)
 
 
 @bp.route("/create", methods=("GET", "POST"))
 def create():
+    if request.method == "POST":
+        author = request.form["author"] or "Anonymous"
+        message = request.form["message"]
+
+        if message:
+            db = get_db()
+            db.execute(
+                "INSERT INTO post(author, message) values (?, ?)",
+                (author, message),
+            )
+            db.commit()
+            return redirect(url_for("posts.posts"))
+
     return render_template("posts/create.html")
 
 
 @bp.route("/posts")
 def posts():
-    posts = []
+    db = get_db()
+    posts = db.execute(
+        "SELECT author, message, created FROM posts ORDER BY created DESC"
+    ).fetchall()
     return render_template("posts/posts.html", posts=posts)
 
