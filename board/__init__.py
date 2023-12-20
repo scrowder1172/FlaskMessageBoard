c142dd9d8ecb806c352bba463b07cb89262dcd13
Scott Crowder <scrowder@gmail.com>
2023-12-19 21:05:41 -0600

    create app factory

--- board/__init__.py
+++ board/__init__.py
@@ -1,13 +1,8 @@
 from flask import Flask
 
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
+    return app
 
-@app.route("/")
-def home():
-    return "Hello, world!"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
